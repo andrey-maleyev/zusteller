f7bc09b37d0fbf9cd0ee723bd5b658e16ae6a42c
Andrey Maleev <andrey.maleyev@gmail.com>
2020-06-06 23:08:05 +0200

    subscriptions index route ctrl view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :districts, only: [:index] do
-    resources :customers, only: [:index]
+    resources :subscriptions, only: [:index]
   end
   resources :newspapers, only: [:index]
 
