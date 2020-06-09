751e4d1e09f0a7a49295d08a58c0655b548896c4
Andrey Maleev <andrey.maleyev@gmail.com>
2020-06-09 22:42:22 +0200

    customers new: route, ctrl action, view, styles

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,12 +1,17 @@
 class CustomersController < ApplicationController
-  before_action :set_district, only: [:index]
+  before_action :set_district, only: [:index, :new]
 
   def index
     @customers = Customer.where(district_id: @district).order(:address)
   end
-   
+
+  def new
+    # we need @district in our `simple_form_for` (views\customers\_form.html.erb), so we use before_action
+    @customer = Customer.new
+  end
+
   private
- 
+
   def set_district
     @district = District.find(params[:district_id])
   end
